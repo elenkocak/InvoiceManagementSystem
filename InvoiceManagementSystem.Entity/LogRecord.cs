e19b6d329c12094a6917a6e5c5579937f12c6343
elen.kocak@coino.com <elen.kocak@coino.com>
2023-05-04 18:18:39 +0300

    fix for log system

---
+++ InvoiceManagementSystem.Entity/LogRecord.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceManagementSystem.Core.Entities;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.Entity
+{
+    //public class LogRecord:IEntity
+    //{
+    
+    //}
+}
